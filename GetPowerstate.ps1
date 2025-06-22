a4e20bc035ab4007473194049a827b81e58c84b6
CA-The-destroyer <126508033+CA-The-destroyer@users.noreply.github.com>
2025-06-22 11:08:13 -0500

    Add files via upload

--- GetPowerstate.ps1
+++ GetPowerstate.ps1
@@ -1,4 +1,3 @@
-Get-AzVM -Status | Where-Object { $_.Zones -contains "3" } | Select-Object Name, ResourceGroupName, Location, Zones, PowerState
-
-
-
+$vms = Get-AzVM -Status | Where-Object { $_.Zones -contains "3" }
+$vms | Select-Object Name, ResourceGroupName, Location, Zones, PowerState
+Write-Host "`nTotal VMs powered on in Zone 3: " ($vms | Where-Object { $_.PowerState -eq 'VM running' }).Count -ForegroundColor Green
